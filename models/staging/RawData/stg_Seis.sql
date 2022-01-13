44f990d3ebcd3031286051ff39ccca0dfb57514f
Brett Noble <bnoble@acecharter.org>
2022-01-13 22:41:37 +0000

    add RawData and Seis source files and initial staging sql code

---
+++ models/staging/RawData/stg_Seis.sql
@@ -0,0 +1,29 @@
+WITH
+seis AS (
+  SELECT * FROM {{ source('RawData', 'SeisEmpower')}}
+  
+  UNION ALL
+  SELECT * FROM {{ source('RawData', 'SeisEsperanza')}}
+  
+  UNION ALL
+  SELECT * FROM {{ source('RawData', 'SeisInspire')}}
+  
+  UNION ALL
+  SELECT * FROM {{ source('RawData', 'SeisHS')}}
+),
+
+final AS (
+  SELECT
+    CAST(SEIS_ID AS STRING) AS SeisId,
+    Last_Name AS LastName,
+    First_Name AS FirstName,
+    Date_of_Birth AS BirthDate,
+    School_of_Attendance AS SchoolName,
+    CAST(Student_SSID AS STRING) AS StateUniqueId,
+    Grade_Code AS GradeLevel,
+    Seis_Extract_Date
+
+  FROM seis
+)
+
+SELECT * FROM final
