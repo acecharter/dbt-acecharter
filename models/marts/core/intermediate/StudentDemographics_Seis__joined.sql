84bf9811cf71e4ff1be8573f836be845e2667ac2
Brett Noble <bnoble@acecharter.org>
2022-01-14 06:19:51 +0000

    Join demographics and SEIS/IEP data in an intermediate file

---
+++ models/marts/core/intermediate/StudentDemographics_Seis__joined.sql
@@ -0,0 +1,18 @@
+WITH student_demographics AS (
+    SELECT * FROM {{ ref('stg_StarterPack__StudentDemographics')}}
+),
+students_with_iep AS (
+    SELECT * FROM {{ ref('stg_RawData__Seis')}}
+    WHERE StudentEligibilityStatus = 'Eligible/Previously Eligible'
+)
+
+SELECT
+ sd.*,
+ CASE
+   WHEN i.SeisId IS NOT NULL THEN TRUE
+   ELSE FALSE
+ END AS HasIep,
+ i.SeisExtractDate AS IepStatusDate 
+FROM student_demographics AS sd
+LEFT JOIN students_with_iep AS i
+USING (StateUniqueId)
