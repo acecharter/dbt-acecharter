11cda59f3cc4874f1ce379fa7c1a1e94bd920782
Brett Noble <bnoble@acecharter.org>
2022-01-22 22:27:07 +0000

    Updated SEIS staging model to use individual school SEIS files as source files

--- models/staging/RawData/stg_RawData__Seis.sql
+++ models/staging/RawData/stg_RawData__Seis.sql
@@ -1,20 +1,32 @@
 /*
-Note: After extracting and uploading individual school SEIS files to BigQuery, 
-these files must be unioned and Seis_Extract_Date column added in BigQuery prior
-using BigQuery saved query 'seis_union_and_add_extract_date' prior to running
-this model.
+Note: After uploading the individual school SEIS files to BigQuery, the 
+Seis_Extract_Date column must be added and populated with a date string
+(using 'YYYY-MM-DD' format) in BigQuery prior to running this model, using
+the saved project query 'Seis_AddExtractDateToSeisSchoolFiles'.
 */
 
-  SELECT
-    CAST(SEIS_ID AS STRING) AS SeisId,
-    Last_Name AS LastName,
-    First_Name AS FirstName,
-    Date_of_Birth AS BirthDate,
-    School_of_Attendance AS SchoolName,
-    CAST(Student_SSID AS STRING) AS StateUniqueId,
-    Grade_Code AS GradeLevel,
-    Student_Eligibility_Status AS StudentEligibilityStatus,
-    Seis_Extract_Date AS SeisExtractDate
+WITH seis AS (
+  SELECT * FROM {{ source('RawData', 'SeisEmpower')}}
 
-FROM {{ source('RawData', 'Seis')}}
+  UNION ALL
+  SELECT * FROM {{ source('RawData', 'SeisEsperanza')}}
+  
+  UNION ALL
+  SELECT * FROM {{ source('RawData', 'SeisInspire')}}
 
+  UNION ALL
+  SELECT * FROM {{ source('RawData', 'SeisHighSchool')}}
+)
+
+SELECT
+  CAST(SEIS_ID AS STRING) AS SeisId,
+  Last_Name AS LastName,
+  First_Name AS FirstName,
+  Date_of_Birth AS BirthDate,
+  School_of_Attendance AS SchoolName,
+  CAST(Student_SSID AS STRING) AS SSID,
+  Grade_Code AS GradeLevel,
+  Student_Eligibility_Status AS StudentEligibilityStatus,
+  CAST(Seis_Extract_Date AS DATE) AS SeisExtractDate
+
+FROM seis
