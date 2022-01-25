996262ca004663781a367bd6afdf06a39fdb9681
Brett Noble <bnoble@acecharter.org>
2022-01-25 18:55:12 +0000

    Add RawData__Schools staging file and incorporated into dim_Schools

--- models/staging/StarterPack/stg_StarterPack__Schools.sql
+++ models/staging/StarterPack/stg_StarterPack__Schools.sql
@@ -1,25 +1,17 @@
-SELECT
-  SchoolId,
-  NameOfInstitution AS SchoolName,
-  CASE
-    WHEN SchoolId = '116814' THEN 'Empower'
-    WHEN SchoolId = '131656' THEN 'Inspire'
-    WHEN SchoolId = '129247' THEN 'Esperanza'
-    WHEN SchoolId = '125617' THEN 'ACE High'
-  END AS SchoolNameShort,
-  CASE
-    WHEN SchoolId = '116814' THEN 'Middle'
-    WHEN SchoolId = '131656' THEN 'Middle'
-    WHEN SchoolId = '129247' THEN 'Middle'
-    WHEN SchoolId = '125617' THEN 'High'
-  END AS SchoolType,
-  PhysicalStreetNumberName,
-  PhysicalCity,
-  PhysicalStateAbbreviation,
-  PhysicalPostalCode,
-  MailingStreetNumberName,
-  MailingCity,
-  MailingStateAbbreviation,
-  MailingPostalCode,
-  GradeLevel
-FROM {{ source('StarterPack', 'Schools')}}
+WITH schools AS (
+  SELECT
+    SchoolId,
+    NameOfInstitution AS SchoolName,
+    PhysicalStreetNumberName,
+    PhysicalCity,
+    PhysicalStateAbbreviation,
+    PhysicalPostalCode,
+    MailingStreetNumberName,
+    MailingCity,
+    MailingStateAbbreviation,
+    MailingPostalCode,
+    GradeLevel
+  FROM {{ source('StarterPack', 'Schools')}}
+)
+
+SELECT * FROM schools
