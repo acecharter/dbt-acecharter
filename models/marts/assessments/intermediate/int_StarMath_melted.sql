e6f7063f56850435e4cbe4e0dc03c5b2a7a5cc55
Brett Noble <bnoble@acecharter.org>
2022-01-28 07:44:12 +0000

    Fixed many errors in int_StarMath_melted

--- models/marts/assessments/intermediate/int_StarMath_melted.sql
+++ models/marts/assessments/intermediate/int_StarMath_melted.sql
@@ -1,7 +1,11 @@
 WITH star_reading AS (
-  SELECT * 
-    ASSESSMENT_KEY,
-    OBJECTIVE_ASSESSMENT_KEY,
+  SELECT * FROM {{ ref('stg_RenaissanceStar__Reading_v2') }}
+),
+
+star_reading_keys AS(
+  SELECT
+    --ASSESSMENT_KEY,
+    --OBJECTIVE_ASSESSMENT_KEY,
     StudentRenaissanceID,
     StudentIdentifier,
     SSID,
@@ -10,89 +14,110 @@ WITH star_reading AS (
     AssessmentID,
     AssessmentDate AS AdministrationDate,
     Grade AS AssessedGradeLevel
-  
+  FROM star_reading
 ),
 
-ge_minus_gp AS (
+gp AS (
   SELECT
     AssessmentID,
     'Grade Equivalent Minus Grade Placement' AS ReportingMethod,
-    'Grade Equivalent points above Grade Placement' AS StudentResultDataType
-    GradeEquivalent - GradePlacement AS StudentResult
-)
+    'Grade Equivalent points above Grade Placement' AS StudentResultDataType,
+    CAST(GradePlacement AS STRING) AS StudentResult
+  FROM star_reading
+),
 
 ge AS (
+  SELECT
     AssessmentID,
     'Grade Equivalent' AS ReportingMethod,
-    'Grade Equivalent' AS StudentResultDataType
-    GradeEquivalent AS StudentResult
+    'Grade Equivalent' AS StudentResultDataType,
+    CAST(GradeEquivalent AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 unified_score AS (
+  SELECT
     AssessmentID,
     'Unified Score' AS ReportingMethod,
-    'Unified Score' AS StudentResultDataType
-    UnifiedScore AS StudentResult
+    'Unified Score' AS StudentResultDataType,
+    CAST(UnifiedScore AS STRING) AS StudentResult
+  FROM star_reading
 ), 
 
 percentile_rank AS (
+  SELECT
     AssessmentID,
     'Percentile Rank' AS ReportingMethod,
-    'Percentile Rank' AS StudentResultDataType
-    PercentileRank AS StudentResult
+    'Percentile Rank' AS StudentResultDataType,
+    CAST(PercentileRank AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 lexile AS (
+  SELECT
     AssessmentID,
     'Lexile Level' AS ReportingMethod,
-    'Lexile Level' AS StudentResultDataType
-    Lexile AS StudentResult
+    'Lexile Level' AS StudentResultDataType,
+    CAST(Lexile AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 sgp_fall_fall AS (
+  SELECT
     AssessmentID,
     'Student Growth Percentile (Fall to Fall)' AS ReportingMethod,
-    'Student Growth Percentile' AS StudentResultDataType
-    StudentGrowthPercentileFallFall AS StudentResult
+    'Student Growth Percentile' AS StudentResultDataType,
+    CAST(StudentGrowthPercentileFallFall AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 sgp_fall_winter AS (
+  SELECT
     AssessmentID,
     'Student Growth Percentile (Fall to Winter)' AS ReportingMethod,
-    'Student Growth Percentile' AS StudentResultDataType
-    StudentGrowthPercentileFallWinter AS StudentResult
+    'Student Growth Percentile' AS StudentResultDataType,
+    CAST(StudentGrowthPercentileFallWinter AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 sgp_fall_spring AS (
+  SELECT
     AssessmentID,
     'Student Growth Percentile (Fall to Spring)' AS ReportingMethod,
-    'Student Growth Percentile' AS StudentResultDataType
-    StudentGrowthPercentileFallSpring AS StudentResult
+    'Student Growth Percentile' AS StudentResultDataType,
+    CAST(StudentGrowthPercentileFallSpring AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 sgp_spring_spring AS (
+  SELECT
     AssessmentID,
     'Student Growth Percentile (Spring to Spring)' AS ReportingMethod,
-    'Student Growth Percentile' AS StudentResultDataType
-    StudentGrowthPercentileSpringSpring AS StudentResult
+    'Student Growth Percentile' AS StudentResultDataType,
+    CAST(StudentGrowthPercentileSpringSpring AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 sgp_winter_spring AS (
+  SELECT
     AssessmentID,
     'Student Growth Percentile (Winter to Spring)' AS ReportingMethod,
-    'Student Growth Percentile' AS StudentResultDataType
-    StudentGrowthPercentileWinterSpring AS StudentResult
+    'Student Growth Percentile' AS StudentResultDataType,
+    CAST(StudentGrowthPercentileWinterSpring AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 sgp_current AS (
+  SELECT
     AssessmentID,
     'Student Growth Percentile (current)' AS ReportingMethod,
-    'Student Growth Percentile' AS StudentResultDataType
-    CurrentSGP AS StudentResult
+    'Student Growth Percentile' AS StudentResultDataType,
+    CAST(CurrentSGP AS STRING) AS StudentResult
+  FROM star_reading
 ),
 
 results_unioned AS(
-  SELECT * FROM ge_minus_gp
+  SELECT * FROM gp
   UNION ALL
   SELECT * FROM ge
   UNION ALL
@@ -119,6 +144,7 @@ results_unioned AS(
 SELECT
  s.*,
  r.*
-FROM star_reading AS s
+FROM star_reading_keys AS s
 LEFT JOIN results_unioned AS r
 USING (AssessmentID)
+WHERE StudentResult IS NOT NULL
