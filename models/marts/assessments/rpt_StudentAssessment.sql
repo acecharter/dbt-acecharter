d05e02784442f459e528faf10a4ed6b5eef11668
Brett Noble <bnoble@acecharter.org>
2022-01-31 22:51:57 +0000

    Add rept_StudentAssessment file and initial code (currently w/ errors)

---
+++ models/marts/assessments/rpt_StudentAssessment.sql
@@ -0,0 +1,21 @@
+WITH current_students AS (
+    SELECT *
+    FROM {{ ref('dim_Students') }}
+    WHERE IsCurrentlyEnrolled = true
+),
+
+assessments AS (
+    SELECT *
+    FROM {{ ref('fct_StudentAssessment')}}
+)
+
+
+SELECT
+  s.* EXCEPT (
+      ExitWithdrawDate,
+      ExitWithdrawReason
+    ),
+  a.*
+FROM current_students AS s
+LEFT JOIN assessments AS a
+USING(StateUniqueId)
