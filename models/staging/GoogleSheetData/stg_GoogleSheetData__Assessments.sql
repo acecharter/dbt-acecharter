ea391208e890d32392de667ba794ce0752c9fab1
Brett Noble <bnoble@acecharter.org>
2022-01-31 18:42:20 +0000

    Updated GoogleSheetData__Assessments staging file due to merging assessment/assessment family source tables

--- models/staging/GoogleSheetData/stg_GoogleSheetData__Assessments.sql
+++ models/staging/GoogleSheetData/stg_GoogleSheetData__Assessments.sql
@@ -1,25 +1,9 @@
-WITH assessments AS (
-  SELECT
-    CAST(AceAssessmentUniqueId AS STRING) AS AceAssessmentUniqueId,
-    AssessmentFamilyId,
-    AssessmentNameLong,
-    AssessmentNameShort,
-    CAST(SystemOrVendorAssessmentId AS STRING) AS SystemOrVendorAssessmentId
-  FROM {{ source('GoogleSheetData', 'Assessments')}}
-),
-
-assessment_families AS(
-  SELECT * FROM {{ source('GoogleSheetData', 'AssessmentFamilies')}}
-)
-
 SELECT
-  a.AceAssessmentUniqueId,
-  a.AssessmentNameLong,
-  a.AssessmentNameShort,
-  a.AssessmentFamilyId,
-  f.AssessmentFamilyName,
-  f.SystemOrVendor AS SystemOrVendorName,
-  a.SystemOrVendorAssessmentId
-FROM assessments AS a
-LEFT JOIN assessment_families AS f
-Using (AssessmentFamilyId)
+  CAST(AceAssessmentId AS STRING) AS AceAssessmentId,
+  AssessmentNameShort,
+  AssessmentName,
+  AssessmentFamilyNameShort,
+  AssessmentFamilyName,
+  SystemOrVendorName,
+  CAST(SystemOrVendorAssessmentId AS STRING) AS SystemOrVendorAssessmentId
+FROM {{ source('GoogleSheetData', 'Assessments')}}
