8cc8fe9b96d8dd1357d2edb6bd04d0cf701defd9
Brett Noble <bnoble@acecharter.org>
2022-01-31 23:23:07 +0000

    Added assessment names to student assessments fact and report tables

--- models/marts/assessments/fct_StudentAssessment.sql
+++ models/marts/assessments/fct_StudentAssessment.sql
@@ -1,4 +1,9 @@
-SELECT
+WITH assessment_names AS (
+  SELECT * FROM {{ ref('stg_GoogleSheetData__Assessments') }}
+),
+
+star_math AS (    
+  SELECT
     AceAssessmentId,
     StateUniqueId,
     SchoolId,
@@ -9,10 +14,11 @@ SELECT
     ReportingMethod,
     StudentResultDataType,
     StudentResult
-FROM {{ ref('int_StarMath__melted') }}
+  FROM {{ ref('int_StarMath__melted') }}
+),
 
-UNION ALL
-SELECT
+star_reading AS (
+  SELECT
     AceAssessmentId,
     StateUniqueId,
     SchoolId,
@@ -23,34 +29,66 @@ SELECT
     ReportingMethod,
     StudentResultDataType,
     StudentResult
-FROM {{ ref('int_StarReading__melted') }}
+  FROM {{ ref('int_StarReading__melted') }}
+),
 
-UNION ALL
-SELECT
+caaspp_2021 AS (
+  SELECT
     AceAssessmentId,
     StateUniqueId,
     SchoolId,
     SchoolYear AS AssessmentSchoolYear,
     AssessmentId,
     CAST(NULL AS STRING) AS AdministrationDate,
-    AssessedGradeLevel
-    RecordType,
+    AssessedGradeLevel,
     ReportingMethod,
     StudentResultDataType,
     StudentResult
-FROM {{ ref('int_TomsCaasppEnrolled2021__melted') }}
+  FROM {{ ref('int_TomsCaasppEnrolled2021__melted') }}
+),
 
-UNION ALL
-SELECT
+elpac_2021 AS (
+  SELECT
     AceAssessmentId,
     StateUniqueId,
     SchoolId,
     SchoolYear AS AssessmentSchoolYear,
     AssessmentId,
     CAST(NULL AS STRING) AS AdministrationDate,
-    AssessedGradeLevel
-    RecordType,
+    AssessedGradeLevel,
     ReportingMethod,
     StudentResultDataType,
     StudentResult
 FROM {{ ref('int_TomsElpacEnrolled2021__melted') }}
+),
+
+unioned_results AS (
+    SELECT * FROM star_math
+    UNION ALL 
+    SELECT * FROM star_reading
+    UNION ALL
+    SELECT * FROM caaspp_2021
+    UNION ALL
+    SELECT * FROM elpac_2021
+
+),
+
+final AS (
+  SELECT
+    n.AceAssessmentId,
+    n.AssessmentNameShort AS AssessmentName,
+    r.StateUniqueId,
+    r.SchoolId,
+    r.AssessmentSchoolYear,
+    r.AssessmentId,
+    r.AdministrationDate,
+    r.AssessedGradeLevel,
+    r.ReportingMethod,
+    r.StudentResultDataType,
+    r.StudentResult
+  FROM unioned_results AS r
+  LEFT JOIN assessment_names AS n
+  USING (AceAssessmentId)
+)
+
+SELECT * FROM final
