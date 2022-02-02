5331af98eaacd3405c1a0e6e3a8328cd642c0b4b
Brett Noble <bnoble@acecharter.org>
2022-02-02 18:51:13 +0000

    Updated StudentAttendance_v2 consolidating absence/attendance columns and adding chronic absence columns

--- models/staging/StarterPack/stg_StarterPack__StudentAttendance_v2.sql
+++ models/staging/StarterPack/stg_StarterPack__StudentAttendance_v2.sql
@@ -6,12 +6,19 @@ SELECT
   StudentUniqueId,
   LastSurname,
   FirstName,
-  AverageDailyAttendance,
-  SumOfAllAbsenceEventDurations,
-  CountOfAllAbsenceEvents,
-  SumOfAllInAttendanceEventDurations,
-  CountOfAllInAttendanceEvents,
+  ROUND(AverageDailyAttendance, 4) AS AverageDailyAttendance,
+  ROUND(1 - AverageDailyAttendance, 4) AS AbsenceRate,
+  CountOfAllAbsenceEvents AS CountOfDaysAbsent,
+  CountOfAllInAttendanceEvents AS CountOfDaysInAttendance,
   CountOfDaysEnrolled,
-  SumOfUnreconciledAbsenceEventDurations,
-  CountOfUnreconciledAbsenceEvents
+  CASE
+      WHEN AverageDailyAttendance > 0.9 THEN 'No'
+      WHEN AverageDailyAttendance <= 0.9 THEN 'Yes'
+  END AS IsChronicallyAbsent,
+  CASE
+    WHEN AverageDailyAttendance >= 0.95 THEN 'Satisfactory'
+    WHEN AverageDailyAttendance > 0.90 AND AverageDailyAttendance < 0.95 THEN 'At Risk'
+    WHEN AverageDailyAttendance > 0.80 AND AverageDailyAttendance <= 0.90 THEN 'Moderate Chronic Absence'
+    WHEN AverageDailyAttendance <= 0.80 THEN 'Severe Chronic Absence'
+  END AS AttendanceRateGroup,
 FROM {{ source('StarterPack', 'StudentAttendance_v2')}}
