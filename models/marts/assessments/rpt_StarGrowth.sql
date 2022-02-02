2f4a8ed7155dadd5728a89aeaf430676a3a7d7b7
Brett Noble <bnoble@acecharter.org>
2022-02-02 00:40:22 +0000

    Add Star growth level columns and testing window columns to rpt_StarGrowth

--- models/marts/assessments/rpt_StarGrowth.sql
+++ models/marts/assessments/rpt_StarGrowth.sql
@@ -1,10 +1,52 @@
 SELECT
   * EXCEPT (StudentResultDataType, StudentResult),
-  CAST(StudentResult AS INT64) AS StudentResult,
+  CAST(StudentResult AS INT) AS StudentResult,
   CASE
-    WHEN CAST(StudentResult AS INT64) >= 35 THEN 'Yes'
+    WHEN CAST(StudentResult AS INT) >= 66 THEN 'High Growth'
+    WHEN
+      CAST(StudentResult AS INT) >= 35 AND
+      CAST(StudentResult AS INT) <= 65
+    THEN 'Average Growth'
+    WHEN CAST(StudentResult AS INT) <= 34 THEN 'Low Growth'
+  END AS GrowthLevel,
+  CASE
+    WHEN CAST(StudentResult AS INT) >= 35 THEN 'Yes'
     ELSE 'No' 
-  END AS AverageOrAboveGrowth
+  END AS AtOrAboveAverageGrowth,
+  CASE
+    WHEN
+      AdministrationDate >= '2021-08-01' AND
+      AdministrationDate <= '2021-08-11'
+    THEN 'Fall (early'
+    WHEN
+      AdministrationDate >= '2021-08-11' AND
+      AdministrationDate <= '2021-09-30'
+    THEN 'Fall'
+    WHEN
+      AdministrationDate >= '2021-10-01' AND
+      AdministrationDate <= '2021-11-30'
+    THEN 'Fall (late)'
+    WHEN
+      AdministrationDate >= '2021-12-01' AND
+      AdministrationDate <= '2022-01-14'
+    THEN 'Winter'
+    WHEN
+      AdministrationDate >= '2022-01-15' AND
+      AdministrationDate <= '2022-03-31'
+    THEN 'Winter (late)'
+    WHEN
+      AdministrationDate >= '2022-04-01' AND
+      AdministrationDate <= '2022-04-14'
+    THEN 'Spring (early)'
+    WHEN
+      AdministrationDate >= '2022-04-15' AND
+      AdministrationDate <= '2022-05-31'
+    THEN 'Spring'
+    WHEN
+      AdministrationDate >= '2022-06-01' AND
+      AdministrationDate <= '2022-07-31'
+    THEN 'Spring (late)'
+  END AS TestingWindow
 FROM {{ ref('rpt_StudentAssessment')}}
 WHERE
   AssessmentName IN ('Star Reading', 'Star Math')
