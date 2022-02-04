957b0e43a50fb93e30982f7e234512969b5a78df
Brett Noble <bnoble@acecharter.org>
2022-02-04 23:10:37 +0000

    Major updates to rpt_CurrentStudentCaasppResults

--- models/marts/assessments/rpt_CurrentStudentCaasppResults.sql
+++ models/marts/assessments/rpt_CurrentStudentCaasppResults.sql
@@ -4,29 +4,13 @@ WITH current_students AS (
     WHERE IsCurrentlyEnrolled = true
 ),
 
-caaspp AS (
-    SELECT * EXCEPT (AdministrationDate)
+caaspp_results AS (
+    SELECT
+      * EXCEPT (AdministrationDate, StudentResult),
+      CAST(StudentResult AS INT64) AS StudentResult
     FROM {{ ref('fct_StudentAssessment')}}
     WHERE
       AceAssessmentId IN ('1', '2')
-),
-
-levels AS (
-    SELECT *
-    FROM caaspp
-    WHERE ReportingMethod = 'Achievement Level'
-),
-
-scale_scores AS (
-    SELECT *
-    FROM caaspp
-    WHERE ReportingMethod = 'Scale Score'
-),
-
-dfs AS (
-    SELECT *
-    FROM caaspp
-    WHERE ReportingMethod = 'Distance From Standard'
 ),
 
 schools AS (
@@ -41,21 +25,10 @@ schools AS (
 
 SELECT
   s.* EXCEPT (SchoolId),
-  cs.* EXCEPT (
-      ExitWithdrawDate,
-      ExitWithdrawReason
-    ),
-  l.* EXCEPT (StateUniqueId, TestedSchoolId, ReportingMethod, StudentResultDataType, StudentResult),
-  l.StudentResult AS AchievementLevel,
-  ss.StudentResult AS ScaleScore,
-  dfs.StudentResult AS DistanceFromStandard
+  cs.* EXCEPT (ExitWithdrawReason),
+  cr.* EXCEPT (StateUniqueId, TestedSchoolId),
 FROM current_students AS cs
-LEFT JOIN levels AS l
-ON cs.StateUniqueId = l.StateUniqueId
+INNER JOIN caaspp_results AS cr
+ON cs.StateUniqueId = cr.StateUniqueId
 LEFT JOIN schools AS s
 ON cs.SchoolId = s.SchoolId
-LEFT JOIN scale_scores AS ss
-ON l.AssessmentId = ss.AssessmentId
-LEFT JOIN dfs
-ON l.AssessmentId = dfs.AssessmentId
-WHERE l.AssessmentId IS NOT NULL
