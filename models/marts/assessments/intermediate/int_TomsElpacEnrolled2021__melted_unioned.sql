596965004c9b5f77c3061e3997a1e006605c9dbd
Brett Noble <bnoble@acecharter.org>
2022-02-05 08:10:19 +0000

    Remove "Overall" from ReportingMethod values in TomsElpacEnrolled melted unioned file

--- models/marts/assessments/intermediate/int_TomsElpacEnrolled2021__melted_unioned.sql
+++ models/marts/assessments/intermediate/int_TomsElpacEnrolled2021__melted_unioned.sql
@@ -6,4 +6,19 @@ WITH final AS(
   SELECT * FROM {{ ref('int_TomsElpacEnrolled2021__melted_2019') }}
 )
 
-SELECT * FROM final
+SELECT
+  AceAssessmentId,
+  StateUniqueId,
+  TestedSchoolId,
+  SchoolYear,
+  AssessmentId,
+  AssessedGradeLevel,
+  RecordType,
+  CASE
+    WHEN ReportingMethod='Overall Performance Level' THEN 'Performance Level'
+    WHEN ReportingMethod='Overall Scale Score' THEN 'Scale Score'
+    ELSE ReportingMethod
+  END AS ReportingMethod,
+  StudentResultDataType,
+  StudentResult
+FROM final
