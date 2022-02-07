81037e1592e23dfd9117808a84029ff4c6069364
Brett Noble <bnoble@acecharter.org>
2022-02-07 02:36:57 +0000

    Add googlesheet with student IDs missing StarRenaissance data

---
+++ models/staging/GoogleSheetData/stg_GoogleSheetData__StarRenaissanceMissingStudentIds.sql
@@ -0,0 +1,5 @@
+SELECT
+  StudentRenaissanceID,
+  CAST(StudentIdentifier AS STRING) AS StudentIdentifier,
+  CAST(StateUniqueId AS STRING) AS StateUniqueId
+FROM {{ source('GoogleSheetData', 'StarRenaissanceMissingStudentIds')}}
