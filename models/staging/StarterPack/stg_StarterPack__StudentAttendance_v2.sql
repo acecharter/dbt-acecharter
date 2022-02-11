08cb287f5d851492b92deb98d5a284faffd851f4
Brett Noble <bnoble@acecharter.org>
2022-02-11 05:08:59 +0000

    Updated chronic absence columns to mark N/A students who haven't attended a single day and who have been enrolled < 31 days

--- models/staging/StarterPack/stg_StarterPack__StudentAttendance_v2.sql
+++ models/staging/StarterPack/stg_StarterPack__StudentAttendance_v2.sql
@@ -12,10 +12,14 @@ SELECT
   CountOfAllInAttendanceEvents AS CountOfDaysInAttendance,
   CountOfDaysEnrolled,
   CASE
+      WHEN CountOfAllInAttendanceEvents = 0 THEN 'N/A (0 days attended)' 
+      WHEN CountOfDaysEnrolled < 31 THEN 'N/A (enrolled <31 days)'
       WHEN AverageDailyAttendance > 0.9 THEN 'No'
       WHEN AverageDailyAttendance <= 0.9 THEN 'Yes'
   END AS IsChronicallyAbsent,
   CASE
+    WHEN CountOfAllInAttendanceEvents = 0 THEN 'N/A (0 days attended)' 
+    WHEN CountOfDaysEnrolled < 31 THEN 'N/A (enrolled <31 days)'
     WHEN AverageDailyAttendance >= 0.95 THEN 'Satisfactory'
     WHEN AverageDailyAttendance > 0.90 AND AverageDailyAttendance < 0.95 THEN 'At Risk'
     WHEN AverageDailyAttendance > 0.80 AND AverageDailyAttendance <= 0.90 THEN 'Moderate Chronic Absence'
