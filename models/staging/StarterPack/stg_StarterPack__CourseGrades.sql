691e7e734e19f8077e77bc96c2d76396f86d203f
Brett Noble <bnoble@acecharter.org>
2022-02-15 19:17:29 +0000

    Add  StarterPack CourseGrades staging model

---
+++ models/staging/StarterPack/stg_StarterPack__CourseGrades.sql
@@ -0,0 +1,30 @@
+/*Fields dropped due to lack of use:
+    - AcademicSubject (all NULL),
+    - PerformanceBaseConversionDescriptor (all NULL)
+*/
+
+
+SELECT
+  SchoolId,
+  NameOfInstitution AS SchoolName,
+  StudentUniqueId,
+  LastSurname,
+  FirstName,
+  SectionIdentifier,
+  StaffUniqueId,
+  StaffDisplayName,
+  StaffClassroomPosition,
+  ClassPeriodName,
+  CourseCode,
+  CourseTitle,
+  AvailableCredits,
+  SessionName,
+  GradingPeriodDescriptor,
+  GradeTypeDescriptor,
+  IsCurrentCourseEnrollment,
+  IsCurrentGradingPeriod,
+  NumericGradeEarned,
+  LetterGradeEarned
+
+FROM {{ source('StarterPack', 'CourseGrades')}}
+WHERE DATE(_PARTITIONTIME) = CURRENT_DATE()
