b29cdb94e8e681f9275cd4e894a23f2a650a4296
Brett Noble <bnoble@acecharter.org>
2022-02-15 20:41:45 +0000

    Add Current and Final Grades reports

--- models/marts/core/rpt_GradesCurrent.sql
+++ models/marts/core/rpt_GradesCurrent.sql
@@ -1,5 +1,21 @@
-SELECT * 
-FROM {{ ref('stg_StarterPack__CourseGrades') }}
-WHERE
-  IsCurrentGradingPeriod = true
-  AND IsCurrentCourseEnrollment = true
+WITH current_grades AS (
+  SELECT *
+  FROM {{ ref('stg_StarterPack__CourseGrades') }}
+  WHERE
+    IsCurrentGradingPeriod = true
+    AND IsCurrentCourseEnrollment = true
+),
+
+student_demographics AS (
+  SELECT *
+  FROM {{ ref('dim_Students') }}
+)
+
+SELECT
+ g.* Except(LastSurname, FirstName),
+ d.* EXCEPT(SchoolId, StudentUniqueId)
+FROM current_grades AS g
+LEFT JOIN student_demographics AS d
+ON
+  g.StudentUniqueId = d.StudentUniqueId
+  AND g.SchoolId = d.SchoolId
--- models/marts/core/rpt_GradesFinal.sql
+++ models/marts/core/rpt_GradesFinal.sql
@@ -1,3 +1,19 @@
-SELECT *
-FROM {{ ref('stg_StarterPack__CourseGrades') }}
-WHERE GradeTypeDescriptor = 'Final'
+WITH final_grades AS (
+    SELECT *
+    FROM {{ ref('stg_StarterPack__CourseGrades') }}
+    WHERE GradeTypeDescriptor = 'Final'
+),
+
+student_demographics AS (
+    SELECT *
+    FROM {{ ref('dim_Students') }}
+)
+
+SELECT
+ g.* Except(LastSurname, FirstName),
+ d.* EXCEPT(SchoolId, StudentUniqueId)
+FROM final_grades AS g
+LEFT JOIN student_demographics AS d
+ON
+  g.StudentUniqueId = d.StudentUniqueId
+  AND g.SchoolId = d.SchoolId
