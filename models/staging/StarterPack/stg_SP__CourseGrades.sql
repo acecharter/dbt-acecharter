bbf8090b187bda03cfcb76c1ced4cd5732463905
Brett Noble <bnoble@acecharter.org>
2022-02-19 03:20:11 +0000

    Reverted fct_CourseGrades  to have separate columns for numeric and letter grades

--- models/staging/StarterPack/stg_SP__CourseGrades.sql
+++ models/staging/StarterPack/stg_SP__CourseGrades.sql
@@ -1,3 +1,5 @@
 SELECT *
 FROM {{ source('StarterPack', 'CourseGrades')}}
-WHERE DATE(_PARTITIONTIME) = CURRENT_DATE('America/Los_Angeles')
+WHERE
+  DATE(_PARTITIONTIME) = CURRENT_DATE('America/Los_Angeles')
+  AND LetterGradeEarned IS NOT NULL
