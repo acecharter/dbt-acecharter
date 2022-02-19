e607126fa8c6196dc7bf341ce1a53375435237e9
Brett Noble <bnoble@acecharter.org>
2022-02-19 05:04:43 +0000

    Add/update Attendance Reporting Periods staging model

--- models/staging/RawData/stg_RD__AttendanceReportingPeriods.sql
+++ models/staging/RawData/stg_RD__AttendanceReportingPeriods.sql
@@ -1,9 +1,9 @@
 SELECT
-  CONCAT(SchoolYear, ' ',LEFT(ReportingPeriodType, 1),CAST(ReportingPeriodNumber AS STRING)) AS ReportingPeriodId,
+  CONCAT(SchoolYear, ' ',LEFT(ReportingPeriodType, 1), CAST(ReportingPeriodNumber AS STRING)) AS AttendanceReportingPeriodUniqueId,
   SchoolYear,
   ReportingPeriodType,
   ReportingPeriodNumber,
-  CONCAT(LEFT(ReportingPeriodType, 1),CAST(ReportingPeriodNumber AS STRING)) AS ReportingPeriodShort,
+  CONCAT(LEFT(ReportingPeriodType, 1), CAST(ReportingPeriodNumber AS STRING)) AS ReportingPeriod,
   DATE(StartDate) AS StartDate,
   DATE(EndDate) AS EndDate
 FROM {{ source('RawData', 'AttendanceReportingPeriods')}}
