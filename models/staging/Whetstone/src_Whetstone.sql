6cd18e88164979ee99abe6552b250aa22efb3a24
Brett Noble <bnoble@acecharter.org>
2022-02-23 23:24:39 +0000

    Add new Whetstone staging model folder and source file for the Landing Zone-managed Whetstone/SchoolMint Grow  integration

---
+++ models/staging/Whetstone/src_Whetstone.sql
@@ -0,0 +1,14 @@
+version: 2
+
+
+sources:
+  - name: Whetstone
+    database: landing-zone-acecharter
+    schema: Whetstone
+    tables:
+      - name: Informals_raw
+      - name: Measurements_raw
+      - name: Observations_raw
+      - name: Rubrics_raw
+      - name: Schools_raw
+      - name: Users_raw
