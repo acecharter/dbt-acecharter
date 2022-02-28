74bc0860e84d66cb0d9440e4b3c2cb369061a0d6
Brett Noble <bnoble@acecharter.org>
2022-02-28 18:30:00 +0000

    add intermediate CAASPP filtered model

---
+++ models/marts/assessments/intermediate/int_Caaspp__filtered.sql
@@ -0,0 +1,64 @@
+WITH 
+  caaspp AS (
+    SELECT * FROM {{ ref('stg_RD__CaasppSantaClaraCounty')}} 
+  ),
+
+  schools AS (
+    SELECT *
+    FROM caaspp
+    WHERE SchoolCode IN (
+      '0116814', --ACE Empower
+      '0125617', --ACE High School
+      '0129247', --ACE Esperanza
+      '0131656', --ACE Inspire
+      '6046197', --Lee Mathson Middle (ARUSD)
+      '6047229', --Bridges Academy (FMSD)
+      '6062103', --Muwekma Ohlone Middle (SJUSD)
+      '4330031'  --Independence High (ESUHSD)
+    )
+  ),
+
+  districts AS (
+    SELECT *
+    FROM caaspp
+    WHERE
+      SchoolCode = '0000000' AND
+      DistrictCode IN (
+        '10439', -- Santa Clara County Office of Education
+        '69369', -- Alum Rock Union
+        '69450', -- Franklin-McKinley
+        '69666', -- San Jose Unified
+        '69427'  -- East Side Union
+      )
+  ),
+
+  county AS (
+    SELECT *
+    FROM caaspp
+    WHERE CountyCode = '43' AND DistrictCode = '00000' -- Santa Clara County
+  ),
+
+  unioned AS (
+    SELECT * FROM schools
+    UNION ALL
+    SELECT * FROM districts
+    UNION ALL
+    SELECT * FROM county
+  ),
+
+  final AS (
+    SELECT *
+    FROM unioned
+    WHERE
+      GradeLevel >= 5 AND
+      DemographicId IN (
+        '1',   --All Students
+        '128', --Reported Disabilities
+        '31',  --Economic disadvantaged
+        '160', --EL (English learner)
+        '78',  --Hispanic or Latino
+        '204'  --Economically disadvantaged Hispanic or Latino
+      )
+  )
+
+SELECT * FROM final
