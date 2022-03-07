46c1b26070b0ec062eea3803f578953b5e969741
Brett Noble <bnoble@acecharter.org>
2022-03-07 20:53:01 +0000

    Modify assessment name source for SBAC interim assessments

--- models/marts/assessments/fct_StudentAssessment.sql
+++ models/marts/assessments/fct_StudentAssessment.sql
@@ -1,62 +1,72 @@
-WITH assessment_names AS (
-  SELECT * FROM {{ ref('stg_GSD__Assessments') }}
-),
+WITH
+  assessment_names AS (
+    SELECT * FROM {{ ref('stg_GSD__Assessments') }}
+  ),
 
-star AS (    
-  SELECT
-    AceAssessmentId,
-    StateUniqueId,
-    TestedSchoolId,
-    SchoolYear AS AssessmentSchoolYear,
-    AssessmentID AS AssessmentId,
-    CAST(AssessmentDate AS STRING) AS AssessmentDate,
-    AssessedGradeLevel,
-    AssessmentObjective,
-    ReportingMethod,
-    StudentResultDataType,
-    StudentResult
-  FROM {{ ref('int_RenStar__unioned_melted') }}
-),
+  star AS (    
+    SELECT
+      s.AceAssessmentId,
+      a.AssessmentNameShort AS AssessmentName,
+      s.StateUniqueId,
+      s.TestedSchoolId,
+      s.SchoolYear AS AssessmentSchoolYear,
+      s.AssessmentID AS AssessmentId,
+      CAST(s.AssessmentDate AS STRING) AS AssessmentDate,
+      s.AssessedGradeLevel,
+      s.AssessmentObjective,
+      s.ReportingMethod,
+      s.StudentResultDataType,
+      s.StudentResult
+    FROM {{ ref('int_RenStar__unioned_melted') }} AS s
+    LEFT JOIN assessment_names AS a
+    USING (AceAssessmentId)
+  ),
 
-cers AS (
-  SELECT
-    AceAssessmentId,
-    StateUniqueId,
-    TestedSchoolId,
-    SchoolYear AS AssessmentSchoolYear,
-    AssessmentId,
-    CAST(AssessmentDate AS STRING) AS AssessmentDate,
-    AssessedGradeLevel,
-    AssessmentObjective,
-    ReportingMethod,
-    StudentResultDataType,
-    StudentResult
-FROM {{ ref('int_Cers__unioned_melted') }}
-),
+  cers AS (
+    SELECT
+      c.AceAssessmentId,
+      CASE
+        WHEN c.AceAssessmentId IN ('15', '16', '17', '18') THEN c.GradeAssessmentName
+        ELSE a.AssessmentNameShort 
+      END AS AssessmentName,
+      c.StateUniqueId,
+      c.TestedSchoolId,
+      c.SchoolYear AS AssessmentSchoolYear,
+      c.AssessmentId,
+      CAST(c.AssessmentDate AS STRING) AS AssessmentDate,
+      c.AssessedGradeLevel,
+      c.AssessmentObjective,
+      c.ReportingMethod,
+      c.StudentResultDataType,
+      c.StudentResult
+    FROM {{ ref('int_Cers__unioned_melted') }} AS c
+    LEFT JOIN assessment_names AS a
+    USING (AceAssessmentId)
+  ),
 
-unioned_results AS (
+  unioned_results AS (
     SELECT * FROM star
     UNION ALL
     SELECT * FROM cers
-),
+  ),
 
-final AS (
-  SELECT
-    n.AceAssessmentId,
-    n.AssessmentNameShort AS AssessmentName,
-    r.StateUniqueId,
-    r.TestedSchoolId,
-    r.AssessmentSchoolYear,
-    r.AssessmentId,
-    r.AssessmentDate,
-    r.AssessedGradeLevel,
-    r.AssessmentObjective,
-    r.ReportingMethod,
-    r.StudentResultDataType,
-    r.StudentResult
-  FROM unioned_results AS r
-  LEFT JOIN assessment_names AS n
-  USING (AceAssessmentId)
-)
+  final AS (
+    SELECT
+      n.AceAssessmentId,
+      r.AssessmentName,
+      r.StateUniqueId,
+      r.TestedSchoolId,
+      r.AssessmentSchoolYear,
+      r.AssessmentId,
+      r.AssessmentDate,
+      r.AssessedGradeLevel,
+      r.AssessmentObjective,
+      r.ReportingMethod,
+      r.StudentResultDataType,
+      r.StudentResult
+    FROM unioned_results AS r
+    LEFT JOIN assessment_names AS n
+    USING (AceAssessmentId)
+  )
 
 SELECT * FROM final
