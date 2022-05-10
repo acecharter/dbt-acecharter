2bd7eb6cb6b45e9c97c25f3e63492a11a454613f
Brett Noble <bnoble@acecharter.org>
2022-05-10 22:03:16 +0000

    Add CDE chronic absenteeism data models

---
+++ models/marts/attendance/rpt_ChronicAbsenteeism.sql
@@ -0,0 +1,1 @@
+SELECT * FROM {{ ref('fct_ChronicAbsenteeism')}}
---
+++ models/staging/RawData/stg_RD__CdeChronicAbsenteeism.sql
@@ -0,0 +1,85 @@
+-- Columns dropped: Filler
+WITH
+  chrabs_1617 AS (
+    SELECT *
+    FROM {{ source('RawData', 'CdeChronicAbsenteeism1617')}}
+  ),
+
+  chrabs_1718 AS (
+    SELECT *
+    FROM {{ source('RawData', 'CdeChronicAbsenteeism1718')}}
+  ),
+
+  chrabs_1819 AS (
+    SELECT *
+    FROM {{ source('RawData', 'CdeChronicAbsenteeism1819')}}
+  ),
+  
+  --Note: no 2019-20 chronic absenteeism file due to the pandemic
+
+  chrabs_2021 AS (
+    SELECT
+      Academic_Year AS AcademicYear,
+      Aggregate_Level AS AggregateLevel,
+      County_Code AS CountyCode,
+      District_Code AS DistrictCode,
+      School_Code AS SchoolCode,
+      County_Name AS CountyName,
+      District_Name AS DistrictName,
+      School_Name AS SchoolName,
+      Charter_School AS CharterSchool,
+      Reporting_Category AS ReportingCategory,
+      ChronicAbsenteeismEligibleCumula,
+      ChronicAbsenteeismCount,
+      ChronicAbsenteeismRate
+    FROM {{ source('RawData', 'CdeChronicAbsenteeism2021')}}
+  ),
+
+  unioned AS (
+    SELECT * FROM chrabs_1617
+    UNION ALL
+    SELECT * FROM chrabs_1718
+    UNION ALL
+    SELECT * FROM chrabs_1819
+    UNION ALL
+    SELECT * FROM chrabs_2021
+  ),
+
+  final AS (
+    SELECT
+      AcademicYear,
+      AggregateLevel,
+      CASE
+        WHEN AggregateLevel = 'T' THEN 'State'
+        WHEN AggregateLevel = 'C' THEN 'County'
+        WHEN AggregateLevel = 'D' THEN 'District'
+        WHEN AggregateLevel = 'S' THEN 'School'
+      END AS EntityType,
+      FORMAT("%02d", CAST(CountyCode AS INT64)) AS CountyCode,
+      FORMAT("%05d", CAST(DistrictCode AS INT64)) AS DistrictCode,
+      FORMAT("%07d", CAST(SchoolCode AS INT64)) AS SchoolCode,
+      CountyName,
+      DistrictName,
+      SchoolName,
+      CharterYN AS CharterSchool,
+      ReportingCategory,
+      CAST(ChronicAbsenteeismEligibleCumula AS INT64) AS ChronicAbsenteeismEligibleCumula,
+      CAST(ChronicAbsenteeismCount AS INT64) AS ChronicAbsenteeismCount,
+      ROUND(CAST(ChronicAbsenteeismRate AS FLOAT64)/100, 3) AS ChronicAbsenteeismRate
+    FROM unioned
+    WHERE
+      AggregateLevel = 'T'
+      OR (
+        AggregateLevel = 'C' 
+        AND CountyCode = 43
+      )
+      OR DistrictCode IN (
+        10439, --SCCOE
+        69369, --ARUSD
+        69450, --FMSD
+        69666, --SJUSD
+        69427 -- ESUHSD
+      )
+  )
+
+SELECT * FROM final
