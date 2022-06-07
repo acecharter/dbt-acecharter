5f51e4bf62280fab44b07a288b4d9d2636d66102
Brett Noble <bnoble@acecharter.org>
2022-06-07 17:14:03 +0000

    Replaced unioning with case when statements in CGR entities dim model

--- models/marts/postsecondary/dim_CgrEntities.sql
+++ models/marts/postsecondary/dim_CgrEntities.sql
@@ -14,58 +14,24 @@ WITH
       SchoolName,
       MAX(AcademicYear) AS MaxAcademicYear
     FROM cgr
-
     GROUP BY 1, 2, 3, 4, 5, 6, 7
   ),
 
-  state AS (
-    SELECT
-      EntityType,
-      '00' AS EntityCode,
-      CountyName AS EntityName
-    FROM entity_names
-    WHERE EntityType = 'State'
-  ),
-
-  county AS (
-    SELECT
-      EntityType,
-      CountyCode AS EntityCode,
-      CountyName AS EntityName
-    FROM entity_names
-    WHERE EntityType = 'County'
-  ),
-
-  district AS (
-    SELECT
-      EntityType,
-      DistrictCode AS EntityCode,
-      DistrictName AS EntityName
-    FROM entity_names
-    WHERE EntityType = 'District'
-  ),
-
-  school AS (
-    SELECT
-      EntityType,
-      SchoolCode AS EntityCode,
-      SchoolName AS EntityName
-    FROM entity_names
-    WHERE EntityType = 'School'
-  ),
-
-  unioned AS (
-    SELECT * FROM state
-    UNION ALL
-    SELECT * FROM county
-    UNION ALL
-    SELECT * FROM district
-    UNION ALL
-    SELECT * FROM school
-  ),
-  
   final AS (
-    SELECT * FROM unioned
+    SELECT
+      EntityType,
+      CASE
+        WHEN EntityType = 'State' THEN '00'
+        WHEN EntityType = 'County' THEN CountyCode
+        WHEN EntityType = 'District' THEN DistrictCode
+        WHEN EntityType = 'School' THEN SchoolCode
+      END AS EntityCode,
+      CASE
+        WHEN EntityType IN ('State', 'County') THEN CountyName
+        WHEN EntityType = 'District' THEN DistrictName
+        WHEN EntityType = 'School' THEN SchoolName
+      END AS EntityName
+    FROM entity_names
   )
 
 SELECT *
