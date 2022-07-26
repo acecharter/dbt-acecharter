86bd29bfac067395f91efff822c71bae2baf2088
Brett Noble <bnoble@acecharter.org>
2022-07-26 23:23:21 +0000

    Add 2019 CA Dashboard Grad Indicator staging model

---
+++ models/staging/RawData/stg_RD__CaDashGrad2019.sql
@@ -0,0 +1,31 @@
+SELECT
+  CAST(cds AS STRING) AS Cds,
+  rtype AS RType,
+  schoolname AS SchoolName,
+  districtname AS DistrictName,
+  countyname AS CountyName,
+  charter_flag AS CharterFlag,
+  coe_flag AS CoeFlag,
+  dass_flag AS DassFlag,
+  studentgroup AS StudentGroup,
+  currnumer AS CurrNumer,
+  currdenom AS CurrDenom,
+  currstatus AS CurrStatus,
+  priornumer AS PriorNumer,
+  priordenom AS PriorDenom,
+  priorstatus AS PriorStatus,
+  fiveyrnumer AS FiveYrNumer,
+  safetynet AS SafetyNet,
+  change AS Change,
+  statuslevel AS StatusLevel,
+  changelevel AS ChangeLevel,
+  color AS Color,
+  box AS Box,
+  reportingyear AS ReportingYear
+FROM {{ source('RawData', 'CaDashGrad2019')}}
+WHERE 
+    rtype = 'X'
+    OR rtype = 'D' AND cds = 43694270000000 --ESUHSD
+    OR rtype = 'S' AND cds = 43694274330031 --Independence HS
+    OR rtype = 'S' AND cds = 43694270125617 --ACE Charter HS
+    
