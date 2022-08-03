e7780842be3110124296b9358d7b786e3657bef2
Brett Noble <bnoble@acecharter.org>
2022-08-03 23:42:48 +0000

    Temporarily hardcoding date in course grades staging model so EOY grades from prior show up on dashboards

--- models/staging/StarterPack/stg_SP__CourseGrades.sql
+++ models/staging/StarterPack/stg_SP__CourseGrades.sql
@@ -1,5 +1,6 @@
 SELECT *
 FROM {{ source('StarterPack', 'CourseGrades')}}
 WHERE
-  DATE(_PARTITIONTIME) = CURRENT_DATE('America/Los_Angeles')
+  --DATE(_PARTITIONTIME) = CURRENT_DATE('America/Los_Angeles')
+  DATE(_PARTITIONTIME) = '2022-06-15' --This is temporary code until the start of the 22-23 school year
   AND LetterGradeEarned IS NOT NULL
