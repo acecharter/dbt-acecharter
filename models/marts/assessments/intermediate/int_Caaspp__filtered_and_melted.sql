fdae0846f83b6bce64cdfd379a58fb1d274b9b0c
Brett Noble <bnoble@acecharter.org>
2022-08-05 00:55:11 +0000

    merged filtered and melted intermediate models into a single int model

--- models/marts/assessments/intermediate/int_Caaspp__filtered_and_melted.sql
+++ models/marts/assessments/intermediate/int_Caaspp__filtered_and_melted.sql
@@ -7,7 +7,17 @@ WITH
     SELECT
       CONCAT(CountyCode, DistrictCode, SchoolCode,'-', TestYear, '-', DemographicId, '-', GradeLevel, '-', TestId) AS AssessmentId,
       *
-    FROM {{ ref('int_Caaspp__1_filtered') }}
+    FROM {{ ref('stg_RD__Caaspp')}}
+    WHERE
+      GradeLevel >= 5
+      AND DemographicId IN (
+        '1',   --All Students
+        '128', --Reported Disabilities
+        '31',  --Economic disadvantaged
+        '160', --EL (English learner)
+        '78',  --Hispanic or Latino
+        '204'  --Economically disadvantaged Hispanic or Latino
+        )
   ),
 
   caaspp_keys AS(
