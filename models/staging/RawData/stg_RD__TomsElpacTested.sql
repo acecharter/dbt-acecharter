68112d8064b34354b81b75623888f826ff0c5b23
Brett Noble <bnoble@acecharter.org>
2022-08-16 20:56:43 +0000

    Cleaned up Toms ELPAC staging and base models

--- models/staging/RawData/stg_RD__TomsElpacTested.sql
+++ models/staging/RawData/stg_RD__TomsElpacTested.sql
@@ -4,9 +4,60 @@ WITH
     UNION ALL SELECT * FROM {{ ref('base_RD__TomsElpacTested2021')}}
   ),
 
-  elpac_ns_removed AS (
+  assessment_ids AS (
+    SELECT 
+      AceAssessmentId,
+      AssessmentNameShort AS AceAssessmentName,
+      AssessmentSubject,
+      SystemOrVendorAssessmentId
+    FROM {{ ref('stg_GSD__Assessments') }}
+    WHERE SystemOrVendorName = 'ELPAC'
+  ),
+
+  elpac_id_added AS (
     SELECT
+      a.AceAssessmentId,
+      a.AceAssessmentName,
+      a.AssessmentSubject,
+      e.*
+    FROM elpac AS e
+    LEFT JOIN assessment_ids AS a
+    ON e.RecordType = a.SystemOrVendorAssessmentId
+  ),
+
+  elpi_levels AS (
+    SELECT * FROM {{ ref('stg_GSD__ElpiLevels') }}
+  ),
+
+  elpac_elpi_added AS(
+    SELECT
+      r.*,
+      e.ElpiLevel,
+      e1.ElpiLevel AS ElpiLevelMinus1,
+      e2.ElpiLevel AS ElpiLevelMinus2
+    FROM elpac_id_added AS r
+    LEFT JOIN elpi_levels AS e
+    ON 
+      r.GradeAssessed = CAST(e.GradeLevel AS STRING) AND
+      CAST(r.OverallScaleScore AS INT64) BETWEEN CAST(e.MinScaleScore AS INT64) AND CAST(e.MaxScaleScore AS INT64)
+    LEFT JOIN elpi_levels AS e1
+    ON 
+      r.GradeAssessedMinus1 = CAST(e1.GradeLevel AS STRING) AND
+      CAST(r.OverallScaleScoreMinus1 AS INT64) BETWEEN CAST(e1.MinScaleScore AS INT64) AND CAST(e1.MaxScaleScore AS INT64)
+    LEFT JOIN elpi_levels AS e2
+    ON 
+      r.GradeAssessedMinus2 = CAST(e2.GradeLevel AS STRING) AND
+      CAST(r.OverallScaleScoreMinus2 AS INT64) BETWEEN CAST(e2.MinScaleScore AS INT64) AND CAST(e2.MaxScaleScore AS INT64)
+ ),
+
+ final AS (
+   SELECT
+      AceAssessmentId,
+      AceAssessmentName,
+      AssessmentSubject,
       RecordType,
+      TestYear,
+      SchoolYear,
       SSID,
       StudentLastName,
       StudentFirstName,
@@ -46,7 +97,7 @@ WITH
       ConditionCode,
       Attemptedness,
       IncludeIndicator,
-      CASE WHEN OverallScaleScore = 'NS' THEN NULL ELSE OverallScaleScore END AS OverallScaleScore,
+      OverallScaleScore,
       OverallPL,
       OralLanguagePL,
       WrittenLanguagePL,
@@ -55,7 +106,7 @@ WITH
       WritingPL,
       AttemptednessMinus1,
       GradeAssessedMinus1,
-      CASE WHEN OverallScaleScoreMinus1 = 'NS' THEN NULL ELSE OverallScaleScoreMinus1 END AS OverallScaleScoreMinus1,
+      OverallScaleScoreMinus1,
       OverallPLMinus1,
       OralLanguagePLMinus1,
       WrittenLanguagePLMinus1,
@@ -65,7 +116,7 @@ WITH
       WritingPLMinus1,
       AttemptednessMinus2,
       GradeAssessedMinus2,
-      CASE WHEN OverallScaleScoreMinus2 = 'NS' THEN NULL ELSE OverallScaleScoreMinus2 END AS OverallScaleScoreMinus2,
+      OverallScaleScoreMinus2,
       OverallPLMinus2,
       OralLanguagePLMinus2,
       WrittenLanguagePLMinus2,
@@ -82,54 +133,11 @@ WITH
       ListeningPLMinus3,
       SpeakingPLMinus3,
       ReadingPLMinus3,
-      WritingPLMinus3
-    FROM elpac
-  ),
-
-  assessment_ids AS (
-    SELECT 
-      AceAssessmentId,
-      AssessmentNameShort AS AceAssessmentName,
-      AssessmentSubject,
-      SystemOrVendorAssessmentId
-    FROM {{ ref('stg_GSD__Assessments') }}
-    WHERE SystemOrVendorName = 'ELPAC'
-  ),
-
-  elpac_id_added AS (
-    SELECT
-      a.AceAssessmentId,
-      a.AceAssessmentName,
-      a.AssessmentSubject,
-      e.*
-    FROM elpac_ns_removed AS e
-    LEFT JOIN assessment_ids AS a
-    ON e.RecordType = a.SystemOrVendorAssessmentId
-  ),
-
-  elpi_levels AS (
-    SELECT * FROM {{ ref('stg_GSD__ElpiLevels') }}
-  ),
-
-  final AS(
-    SELECT
-      r.*,
-      e.ElpiLevel,
-      e1.ElpiLevel AS ElpiLevelMinus1,
-      e2.ElpiLevel AS ElpiLevelMinus2
-    FROM elpac_id_added AS r
-    LEFT JOIN elpi_levels AS e
-    ON 
-      r.GradeAssessed = CAST(e.GradeLevel AS STRING) AND
-      CAST(r.OverallScaleScore AS INT64) BETWEEN CAST(e.MinScaleScore AS INT64) AND CAST(e.MaxScaleScore AS INT64)
-    LEFT JOIN elpi_levels AS e1
-    ON 
-      r.GradeAssessedMinus1 = CAST(e1.GradeLevel AS STRING) AND
-      CAST(r.OverallScaleScoreMinus1 AS INT64) BETWEEN CAST(e1.MinScaleScore AS INT64) AND CAST(e1.MaxScaleScore AS INT64)
-    LEFT JOIN elpi_levels AS e2
-    ON 
-      r.GradeAssessedMinus2 = CAST(e2.GradeLevel AS STRING) AND
-      CAST(r.OverallScaleScoreMinus2 AS INT64) BETWEEN CAST(e2.MinScaleScore AS INT64) AND CAST(e2.MaxScaleScore AS INT64)
-)
+      WritingPLMinus3,
+      ElpiLevel,
+      ElpiLevelMinus1,
+      ElpiLevelMinus2
+    FROM elpac_elpi_added
+  )
 
 SELECT * FROM final
