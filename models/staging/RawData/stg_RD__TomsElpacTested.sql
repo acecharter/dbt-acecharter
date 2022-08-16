5fc550a483926f178e111bb0ba973b8a805ed0b0
Brett Noble <bnoble@acecharter.org>
2022-08-16 21:50:18 +0000

    Added additional ELPI change columns to TOMS ELPAC tested staging model

--- models/staging/RawData/stg_RD__TomsElpacTested.sql
+++ models/staging/RawData/stg_RD__TomsElpacTested.sql
@@ -29,7 +29,7 @@ WITH
     SELECT * FROM {{ ref('stg_GSD__ElpiLevels') }}
   ),
 
-  elpac_elpi_added AS(
+  elpac_elpi_level_added AS(
     SELECT
       r.*,
       e.ElpiLevel,
@@ -41,6 +41,9 @@ WITH
       e2.ElpiLevel AS ElpiLevelMinus2,
       e2.ElpiLevelNumeric AS ElpiLevelNumericMinus2,
       e2.ElpiLevelRank As ElpiLevelRankMinus2,
+      e3.ElpiLevel AS ElpiLevelMinus3,
+      e3.ElpiLevelNumeric AS ElpiLevelNumericMinus3,
+      e3.ElpiLevelRank As ElpiLevelRankMinus3,
     FROM elpac_id_added AS r
     LEFT JOIN elpi_levels AS e
     ON 
@@ -54,7 +57,38 @@ WITH
     ON 
       r.GradeAssessedMinus2 = CAST(e2.GradeLevel AS STRING) AND
       CAST(r.OverallScaleScoreMinus2 AS INT64) BETWEEN CAST(e2.MinScaleScore AS INT64) AND CAST(e2.MaxScaleScore AS INT64)
- ),
+    LEFT JOIN elpi_levels AS e3
+    ON 
+      r.GradeAssessedMinus3 = CAST(e3.GradeLevel AS STRING) AND
+      CAST(r.OverallScaleScoreMinus3 AS INT64) BETWEEN CAST(e3.MinScaleScore AS INT64) AND CAST(e3.MaxScaleScore AS INT64)
+  ),
+
+  elpi_change_added AS (
+    SELECT
+      *,
+      ElpiLevelRank - ElpiLevelRankMinus1 AS ElpiLevelChange,
+      CASE
+        WHEN ElpiLevelNumeric = ElpiLevelNumeric THEN CONCAT('Maintained at ', ElpiLevel)
+        When ElpiLevelNumeric > ElpiLevelNumericMinus1 THEN 'Increased'
+        WHEN ElpiLevelNumeric < ElpiLevelNumericMinus1 THEN 'Declined'
+      END AS ElpiChangeCategory,
+      (ElpiLevelNumeric = 4 AND ElpiLevelNumericMinus1 = 4) OR ElpiLevelNumeric - ElpiLevelNumericMinus1 > 0 AS ElpiProgress,
+      ElpiLevelRankMinus1 - ElpiLevelRankMinus2 AS ElpiLevelChangeMinus1,
+      CASE
+        WHEN ElpiLevelNumericMinus1 = ElpiLevelNumericMinus2 THEN CONCAT('Maintained at ', ElpiLevelMinus1)
+        When ElpiLevelNumericMinus1 > ElpiLevelNumericMinus2 THEN 'Increased'
+        WHEN ElpiLevelNumericMinus1 < ElpiLevelNumericMinus2 THEN 'Declined'
+      END AS ElpiChangeCategoryMinus1,
+      (ElpiLevelNumericMinus1 = 4 AND ElpiLevelNumericMinus2 = 4) OR ElpiLevelNumericMinus1 - ElpiLevelNumericMinus2 > 0 AS ElpiProgressMinus1,
+      ElpiLevelRankMinus2 - ElpiLevelRankMinus3 AS ElpiLevelChangeMinus2,
+      CASE
+        WHEN ElpiLevelNumericMinus2 = ElpiLevelNumericMinus3 THEN CONCAT('Maintained at ', ElpiLevelMinus2)
+        When ElpiLevelNumericMinus2 > ElpiLevelNumericMinus3 THEN 'Increased'
+        WHEN ElpiLevelNumericMinus2 < ElpiLevelNumericMinus3 THEN 'Declined'
+      END AS ElpiChangeCategoryMinus2,
+      (ElpiLevelNumericMinus2 = 4 AND ElpiLevelNumericMinus3 = 4) OR ElpiLevelNumericMinus2 - ElpiLevelNumericMinus3 > 0 AS ElpiProgressMinus2,
+    FROM elpac_elpi_level_added
+  ),
 
  final AS (
    SELECT
@@ -143,13 +177,22 @@ WITH
       ElpiLevel,
       ElpiLevelNumeric,
       ElpiLevelRank,
+      ElpiLevelChange,
+      ElpiChangeCategory,
+      ElpiProgress,
       ElpiLevelMinus1,
       ElpiLevelNumericMinus1,
       ElpiLevelRankMinus1,
+      ElpiLevelChangeMinus1,
+      ElpiChangeCategoryMinus1,
+      ElpiProgressMinus1,
       ElpiLevelMinus2
       ElpiLevelNumericMinus2,
       ElpiLevelRankMinus2,
-    FROM elpac_elpi_added
+      ElpiLevelChangeMinus2,
+      ElpiChangeCategoryMinus2,
+      ElpiProgressMinus2,
+    FROM elpi_change_added
   )
 
 SELECT * FROM final
