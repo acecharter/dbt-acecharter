d47d1f77361dec3c05dcae7a55f2ae95e3dd93d2
Brett Noble <bnoble@acecharter.org>
2022-08-27 03:01:27 +0000

    Add SY22 data and SY column to fct_StudentSchoolEnrollments

--- models/marts/core/fct_StudentSchoolEnrollments.sql
+++ models/marts/core/fct_StudentSchoolEnrollments.sql
@@ -1,9 +1,20 @@
-SELECT
-  SchoolId,
-  StudentUniqueId,
-  GradeLevel,
-  EntryDate,
-  ExitWithdrawDate,
-  ExitWithdrawReason,
-  IsCurrentEnrollment
-FROM {{ ref('stg_SP__StudentEnrollments') }}
+WITH unioned AS (
+  SELECT * FROM {{ ref('stg_SP__StudentEnrollments') }}
+  UNION ALL
+  SELECT * FROM {{ ref('stg_SPA__StudentEnrollments_SY22') }}
+),
+
+final AS (
+  SELECT
+    SchoolYear,
+    SchoolId,
+    StudentUniqueId,
+    GradeLevel,
+    EntryDate,
+    ExitWithdrawDate,
+    ExitWithdrawReason,
+    IsCurrentEnrollment
+  FROM unioned
+)
+
+SELECT * FROM final
