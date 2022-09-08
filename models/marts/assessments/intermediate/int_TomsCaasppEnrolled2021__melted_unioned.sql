fa235fd25ede89f380d469f4136411627a40f4c1
Brett Noble <bnoble@acecharter.org>
2022-09-08 22:33:29 +0000

    Deleted TomsEnrolled files as sources and corresponding models

--- models/marts/assessments/intermediate/int_TomsCaasppEnrolled2021__melted_unioned.sql
+++
@@ -1,8 +0,0 @@
-WITH final AS(
-  SELECT * FROM {{ ref('int_TomsCaasppEnrolled2021__melted_2021') }}
-  UNION ALL
-  SELECT * FROM {{ ref('int_TomsCaasppEnrolled2021__melted_2019') }}
-)
-
-SELECT *
-FROM final
