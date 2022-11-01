5378a2b6db3be9596bb70b024027562703bd8d46
Brett Noble <bnoble@acecharter.org>
2022-11-01 17:20:20 +0000

    Add 2017 Grad dashboard data to base layer and incorporate into CaDashGrad staging model

--- models/staging/RawData/stg_RD__CaDashGrad.sql
+++ models/staging/RawData/stg_RD__CaDashGrad.sql
@@ -55,10 +55,40 @@ WITH
     FROM {{ ref('base_RD__CaDashGrad2018')}} 
   ),
 
+  grad_2017 AS (
+    SELECT
+      Cds,
+      RType,
+      SchoolName,
+      DistrictName,
+      CountyName,
+      CharterFlag,
+      CoeFlag,
+      CAST(NULL AS BOOL) AS DassFlag,
+      StudentGroup,
+      CurrNumer,
+      CurrDenom,
+      CurrStatus,
+      PriorNumer,
+      PriorDenom,
+      PriorStatus,
+      NULL AS FiveYrNumer,
+      SafetyNet,
+      Change,
+      StatusLevel,
+      ChangeLevel,
+      Color,
+      Box,
+      ReportingYear
+    FROM {{ ref('base_RD__CaDashGrad2017')}} 
+  ),
+
   unioned AS (
+    SELECT * FROM grad_2019
+    UNION ALL
     SELECT * FROM grad_2018
     UNION ALL
-    SELECT * FROM grad_2019
+    SELECT * FROM grad_2017
   ),
 
   unioned_w_entity_codes AS (
