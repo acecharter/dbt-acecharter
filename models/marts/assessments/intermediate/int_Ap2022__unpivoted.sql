54ab440fb44d835f7680bb2bc2bad8629080328c
Brett Noble <bnoble@acecharter.org>
2023-01-25 22:41:52 +0000

    Fix int_Ap2022__unpivoted model

--- models/marts/assessments/intermediate/int_Ap2022__unpivoted.sql
+++ models/marts/assessments/intermediate/int_Ap2022__unpivoted.sql
@@ -25,7 +25,7 @@ with unpivoted as (
     ) }}
   ),
 
-  filtered as (
+  final as (
     select
       * EXCEPT(FieldName, ValueName),
       SUBSTR(FieldName, 1, LENGTH(FieldName)-2) as FieldName,
@@ -35,69 +35,6 @@ with unpivoted as (
     where
       ValueName is not null
       and trim(ValueName) != ''
-  ),
-
-  ay as (
-    select
-      ApId,
-      ValueName as AdminYear
-    from filtered
-    where FieldName = 'AdminYear'
-  ),
-
-  ec as (
-    select
-      ApId,
-      ValueName as ExamCode
-    from filtered
-    where FieldName = 'ExamCode'
-  ),
-
-  eg as (
-    select
-      ApId,
-      ValueName as ExamGrade
-    from filtered
-    where FieldName = 'ExamGrade'
-  ),
-
-  ic1 as (
-    select
-      ApId,
-      ValueName as IrregularityCode1
-    from filtered
-    where FieldName = 'IrregularityCode1'
-  ),
-
-  ic2 as (
-    select
-      ApId,
-      ValueName as IrregularityCode1
-    from filtered
-    where FieldName = 'IrregularityCode2'
-  ),
-
-  final as (
-      select
-        o.* EXCEPT(FieldName, FieldNameGroup, ValueName),
-        ay.AdminYear,
-        ec.ExamCode,
-        eg.ExamGrade,
-        ic1.IrregularityCode1,
-        ic2.IrregularityCode2
-      from filtered as o
-      left join ay
-      on  o.ApId = ay.ApId
-      left join ec
-      on  o.ApId = ec.ApId
-      left join eg
-      on  o.ApId = eg.ApId
-      left join ic1
-      on  o.ApId = ic1.ApId
-      left join ic2
-      on  o.ApId = ic2.ApId
-      
   )
-  
+
 select * from final
-limit 11111111111111
