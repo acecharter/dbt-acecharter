1a6ea187d2939f37fd3c05225d36191dc63531fb
bnoble@acecharter.org <bnoble@acecharter.org>
2023-06-01 04:49:54 +0000

    Fix source reference errors in TOMS CAASPP Tested 2022 models

--- models/staging/RawData/base/base_RD__TomsCaasppTested2022.sql
+++ models/staging/RawData/base/base_RD__TomsCaasppTested2022.sql
@@ -1,9 +1,9 @@
 WITH
     caaspp AS (
-        SELECT * FROM {{ ref('base_RD__TomsCaasppTested2023Empower')}}
-        UNION ALL SELECT * FROM {{ ref('base_RD__TomsCaasppTested2023Esperanza')}}
-        UNION ALL SELECT * FROM {{ ref('base_RD__TomsCaasppTested2023Inspire')}}
-        UNION ALL SELECT * FROM {{ ref('base_RD__TomsCaasppTested2023HighSchool')}}
+        SELECT * FROM {{ ref('base_RD__TomsCaasppTested2022Empower')}}
+        UNION ALL SELECT * FROM {{ ref('base_RD__TomsCaasppTested2022Esperanza')}}
+        UNION ALL SELECT * FROM {{ ref('base_RD__TomsCaasppTested2022Inspire')}}
+        UNION ALL SELECT * FROM {{ ref('base_RD__TomsCaasppTested2022HighSchool')}}
     ),
 
     final AS (
