1cce6a711705d59a031cfa6ec73189ad83aeddd9
bnoble@acecharter.org <bnoble@acecharter.org>
2023-06-23 17:21:01 +0000

    Moved adding RaceEthnicity from int_CdeEnrByRaceAndGrade__3_unpivoted to in_CdeEnrByRaceAndGrade__1_unioned

--- models/intermediate/int_CdeEnrByRaceAndGrade__1_unioned.sql
+++ models/intermediate/int_CdeEnrByRaceAndGrade__1_unioned.sql
@@ -1,13 +1,13 @@
-WITH 
-  school_enr AS (
-    SELECT
+with 
+  school_enr as (
+    select
       Year,
       CdsCode,
-      SchoolCode AS EntityCode,
-      'School' AS EntityType,
-      School AS EntityName,
-      'All Students' AS Subgroup,
-      'All Schools' AS SchoolType,
+      SchoolCode as EntityCode,
+      'School' as EntityType,
+      School as EntityName,
+      'All Students' as Subgroup,
+      'All Schools' as SchoolType,
       RaceEthnicCode,
       Gender,
       KDGN,
@@ -27,31 +27,31 @@ WITH
       UNGR_SEC,
       ENR_TOTAL,
       ADULT
-    FROM {{ ref('stg_RD__CdeEnr')}}
+    from {{ ref('stg_RD__CdeEnr')}}
   ),
 
-  entity_enr AS (
-    SELECT
-      CAST(LEFT(SchoolYear, 4) AS INT64) AS Year,
-      CAST(NULL AS STRING) AS CdsCode,
+  entity_enr as (
+    select
+      cast(left(SchoolYear, 4) as int64) as Year,
+      cast(null as string) as CdsCode,
       EntityCode,
       EntityType,
       EntityName,
       Subgroup,
       SchoolType,
-      CASE
-        WHEN Ethnicity = 'Not Reported' THEN '0'
-        WHEN Ethnicity = 'American Indian or Alaska Native' THEN '1'
-        WHEN Ethnicity = 'Asian' THEN '2'
-        WHEN Ethnicity = 'Pacific Islander'  THEN '3'
-        WHEN Ethnicity = 'Filipino' THEN '4'
-        WHEN Ethnicity = 'Hispanic or Latino' THEN '5'
-        WHEN Ethnicity = 'African American' THEN '6'
-        WHEN Ethnicity = 'White' THEN '7'
-        WHEN Ethnicity = 'Two or More Races' THEN '9'
-        ELSE 'ERROR'
-      END AS RaceEthnicCode,
-      'All' AS Gender,
+      case
+        when Ethnicity = 'Not Reported' then '0'
+        when Ethnicity = 'American Indian or Alaska Native' then '1'
+        when Ethnicity = 'Asian' then '2'
+        when Ethnicity = 'Pacific Islander'  then '3'
+        when Ethnicity = 'Filipino' then '4'
+        when Ethnicity = 'Hispanic or Latino' then '5'
+        when Ethnicity = 'African American' then '6'
+        when Ethnicity = 'White' then '7'
+        when Ethnicity = 'Two or More Races' then '9'
+        else 'ERROR'
+      end as RaceEthnicCode,
+      'All' as Gender,
       GR_K,
       GR_1,
       GR_2,
@@ -68,16 +68,60 @@ WITH
       GR_12,
       UNGR_SEC,
       ENR_TOTAL,
-      CAST(NULL AS INT64) AS ADULT
-    FROM {{ ref('stg_GSD__CdeEnrByRaceAndGradeEntities')}}
+      cast(null as int64) as ADULT
+    from {{ ref('stg_GSD__CdeEnrByRaceAndGradeEntities')}}
   ),
 
-  final AS (
-    SELECT * FROM school_enr
-    UNION ALL
-    SELECT * FROM entity_enr
+  unioned as (
+    select * from school_enr
+    union all
+    select * from entity_enr
+  ),
+
+  final as (
+    select
+      Year,
+      CdsCode,
+      EntityCode,
+      EntityType,
+      EntityName,
+      Subgroup,
+      SchoolType,
+      RaceEthnicCode,
+      case
+        when RaceEthnicCode = '0' then 'Not Reported'
+        when RaceEthnicCode = '1' then 'American Indian or Alaska Native'
+        when RaceEthnicCode = '2' then 'Asian'
+        when RaceEthnicCode = '3' then 'Pacific Islander'
+        when RaceEthnicCode = '4' then 'Filipino'
+        when RaceEthnicCode = '5' then 'Hispanic or Latino'
+        when RaceEthnicCode = '6' then 'African American'
+        when RaceEthnicCode = '7' then 'White'
+        when RaceEthnicCode = '8' then 'Multiple or No Response (pre-2009)'
+        when RaceEthnicCode = '9' then 'Two or More Races'
+        else 'ERROR'
+      end as RaceEthnicity, 
+      Gender,
+      GR_K,
+      GR_1,
+      GR_2,
+      GR_3,
+      GR_4,
+      GR_5,
+      GR_6,
+      GR_7,
+      GR_8,
+      UNGR_ELEM,
+      GR_9,
+      GR_10,
+      GR_11,
+      GR_12,
+      UNGR_SEC,
+      ENR_TOTAL,
+      ADULT
+    from unioned
   )
 
 SELECT *
 FROM final
-ORDER BY Year, EntityCode, RaceEthnicCode, Gender
+ORDER BY Year, EntityCode, RaceEthnicity, Gender
