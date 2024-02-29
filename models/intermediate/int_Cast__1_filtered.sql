769bb56efbdc5c5240f1c402a01fda0b82cb2769
bnoble@acecharter.org <bnoble@acecharter.org>
2024-02-29 18:44:29 +0000

    Include additional subgroups when filtering CAASPP and CAST models

--- models/intermediate/int_Cast__1_filtered.sql
+++ models/intermediate/int_Cast__1_filtered.sql
@@ -6,9 +6,13 @@ where
     GradeLevel >= 5
     and DemographicId in (
         '1',   --All Students
-        '128', --Reported Disabilities
+        '128', --Reported disabilities
+        '99',  --No reported disabilities
         '31',  --Economic disadvantaged
+        '111', --Not economically disadvantaged
         '160', --EL (English learner)
+        '8',   --RFEP (Reclassified fluent English proficient)
+        '6',   --IFEP and EO (Initial fluent English proficient and English only)
         '78',  --Hispanic or Latino
         '204'  --Economically disadvantaged Hispanic or Latino
     )
