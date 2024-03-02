f6aa48b0ef6d66571d36b1253256d6d9013d6968
bnoble@acecharter.org <bnoble@acecharter.org>
2024-03-02 01:09:32 +0000

    Incorporate recent RFEP and EL/recentRFEP group results for ACE into rpt_EntityCaaspp

--- models/marts/assessments/rpt_EntityCaaspp.sql
+++ models/marts/assessments/rpt_EntityCaaspp.sql
@@ -1,1 +1,15 @@
-select * from {{ ref('fct_EntityCaaspp') }}
+with entity_caaspp as (
+    select * from {{ ref('fct_EntityCaaspp') }}
+),
+
+recent_rfep as (
+    select * from {{ ref('int_TomsCaasppTestedResults__recent_rfep_aggregated')}}
+),
+
+final as (
+    select * from entity_caaspp
+    union all
+    select * from recent_rfep
+)
+
+select * from final
